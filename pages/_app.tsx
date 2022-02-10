ebc575ad028ac3c36f68316fcb4ed246611f2ecf
nilsreich <nilsreich@gmail.com>
2022-02-10 10:38:13 +0000

    asd

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,14 +13,10 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 
   const updateSong = (source) => {
     if (audioRef.current) {
-      //// @ts-ignore
-      audioRef.current.pause();
-      //// @ts-ignore
+      audioRef.currentasd.pause();
       audioRef.current.load();
-      //// @ts-ignore
       audioRef.current.play();
     }
-    //// @ts-ignore
     audioRef.current.src = source
   }
 
